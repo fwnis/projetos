d77524270c2a9d263d0bcf250915dd805e7defb6
Eduardo Fanis <eduardo.fanis@hotmail.com>
2023-02-18 10:10:07 -0300

    header fixo

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// MENU MOBILE
+
 const headerMobile = document.getElementById("header-mobile");
 
 function toggleMenu(event) {
@@ -10,3 +12,20 @@ function toggleMenu(event) {
 
 headerMobile.addEventListener("click", toggleMenu);
 headerMobile.addEventListener("touchstart", toggleMenu);
+
+// HEADER FIXO
+
+window.onscroll = function () {
+  headerFixo();
+};
+
+var header = document.getElementById("header-fixo");
+var sticky = header.offsetTop;
+
+function headerFixo() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixo");
+  } else {
+    header.classList.remove("fixo");
+  }
+}
