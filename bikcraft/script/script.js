3b0037cf752052c11f3e8dd908b84832958dc990
Eduardo Fanis <109631984+fwnis@users.noreply.github.com>
2023-02-27 18:22:34 -0300

    otimizado

--- bikcraft/script/script.js
+++ bikcraft/script/script.js
@@ -1,5 +1,4 @@
 // MENU MOBILE
-
 const headerMobile = document.getElementById("header-mobile");
 
 function toggleMenu(event) {
@@ -14,7 +13,6 @@ headerMobile.addEventListener("click", toggleMenu);
 headerMobile.addEventListener("touchstart", toggleMenu);
 
 // HEADER FIXO
-
 window.onscroll = function () {
   headerFixo();
 };
@@ -28,4 +26,69 @@ function headerFixo() {
   } else {
     header.classList.remove("fixo");
   }
+}
+
+// PÁGINA SELECIONADA
+const links = document.querySelectorAll("#header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ITENS DO ORCAMENTO
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// ABRIR PERGUNTAS FREQUENTES
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// GALERIA BICICLETAS
+const imagens = document.querySelectorAll(".bicicleta-imagens li img");
+
+function galeriaTrocar(event) {
+  const principal = document.querySelector(".bicicleta-principal")
+  const clicada = event.currentTarget;
+  principal.src = clicada.src
+  principal.alt = clicada.alt
+}
+
+function galeriaClique(imagem) {
+  imagem.addEventListener("click", galeriaTrocar);
+}
+
+imagens.forEach(galeriaClique);
+
+// ANIMACAO
+if(window.SimpleAnime) {
+  new SimpleAnime();
 }
